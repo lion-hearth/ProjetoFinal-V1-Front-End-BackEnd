3d280a5913469af5b3a72e176f0c9989432eaeca
Lion Hearth <richard.oliveira1337@gmail.com>
2023-12-14 07:20:08 -0300

    backend

---
+++ als-backend/src/test/java/com/als/AlsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.als;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
